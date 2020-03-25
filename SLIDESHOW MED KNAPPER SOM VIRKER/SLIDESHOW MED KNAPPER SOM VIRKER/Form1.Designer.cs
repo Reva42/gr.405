4850f68ed4929e24b741fd0a589f356eda0fe786
Troldning <62355367+Troldning@users.noreply.github.com>
2020-03-25 20:25:32 +0100

    Add files via upload

---
+++ SLIDESHOW MED KNAPPER SOM VIRKER/SLIDESHOW MED KNAPPER SOM VIRKER/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SLIDESHOW_MED_KNAPPER_SOM_VIRKER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.prevKnap = new System.Windows.Forms.Button();
+            this.nextKnap = new System.Windows.Forms.Button();
+            this.halvcirkelknap1 = new SLIDESHOW_MED_KNAPPER_SOM_VIRKER.Halvcirkelknap();
+            this.halvcirkelknap2 = new SLIDESHOW_MED_KNAPPER_SOM_VIRKER.Halvcirkelknap();
+            this.halvcirkelknap3 = new SLIDESHOW_MED_KNAPPER_SOM_VIRKER.Halvcirkelknap();
+            this.halvcirkelknap4 = new SLIDESHOW_MED_KNAPPER_SOM_VIRKER.Halvcirkelknap();
+            this.halvcirkelknap5 = new SLIDESHOW_MED_KNAPPER_SOM_VIRKER.Halvcirkelknap();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(238, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(602, 325);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Ligger ved mor-800px.jpg");
+            this.imageList1.Images.SetKeyName(1, "Løver i sne-800px.jpg");
+            this.imageList1.Images.SetKeyName(2, "Løveunger-800px.jpg");
+            this.imageList1.Images.SetKeyName(3, "Parring0_-800px.jpg");
+            // 
+            // prevKnap
+            // 
+            this.prevKnap.BackColor = System.Drawing.Color.Transparent;
+            this.prevKnap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.prevKnap.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.prevKnap.ForeColor = System.Drawing.Color.Transparent;
+            this.prevKnap.Location = new System.Drawing.Point(197, 88);
+            this.prevKnap.Name = "prevKnap";
+            this.prevKnap.Size = new System.Drawing.Size(54, 325);
+            this.prevKnap.TabIndex = 1;
+            this.prevKnap.Text = "button1";
+            this.prevKnap.UseVisualStyleBackColor = false;
+            this.prevKnap.Click += new System.EventHandler(this.prevKnap_Click_1);
+            // 
+            // nextKnap
+            // 
+            this.nextKnap.Location = new System.Drawing.Point(786, 88);
+            this.nextKnap.Name = "nextKnap";
+            this.nextKnap.Size = new System.Drawing.Size(54, 325);
+            this.nextKnap.TabIndex = 2;
+            this.nextKnap.Text = "button2";
+            this.nextKnap.UseVisualStyleBackColor = true;
+            this.nextKnap.Click += new System.EventHandler(this.nextKnap_Click);
+            // 
+            // halvcirkelknap1
+            // 
+            this.halvcirkelknap1.Location = new System.Drawing.Point(48, 421);
+            this.halvcirkelknap1.Name = "halvcirkelknap1";
+            this.halvcirkelknap1.Size = new System.Drawing.Size(148, 119);
+            this.halvcirkelknap1.TabIndex = 3;
+            this.halvcirkelknap1.Text = "halvcirkelknap1";
+            this.halvcirkelknap1.UseVisualStyleBackColor = true;
+            this.halvcirkelknap1.Click += new System.EventHandler(this.halvcirkelknap1_Click_1);
+            // 
+            // halvcirkelknap2
+            // 
+            this.halvcirkelknap2.Location = new System.Drawing.Point(252, 419);
+            this.halvcirkelknap2.Name = "halvcirkelknap2";
+            this.halvcirkelknap2.Size = new System.Drawing.Size(148, 119);
+            this.halvcirkelknap2.TabIndex = 4;
+            this.halvcirkelknap2.Text = "halvcirkelknap2";
+            this.halvcirkelknap2.UseVisualStyleBackColor = true;
+            // 
+            // halvcirkelknap3
+            // 
+            this.halvcirkelknap3.Location = new System.Drawing.Point(448, 421);
+            this.halvcirkelknap3.Name = "halvcirkelknap3";
+            this.halvcirkelknap3.Size = new System.Drawing.Size(148, 119);
+            this.halvcirkelknap3.TabIndex = 5;
+            this.halvcirkelknap3.Text = "halvcirkelknap3";
+            this.halvcirkelknap3.UseVisualStyleBackColor = true;
+            // 
+            // halvcirkelknap4
+            // 
+            this.halvcirkelknap4.Location = new System.Drawing.Point(648, 421);
+            this.halvcirkelknap4.Name = "halvcirkelknap4";
+            this.halvcirkelknap4.Size = new System.Drawing.Size(148, 119);
+            this.halvcirkelknap4.TabIndex = 6;
+            this.halvcirkelknap4.Text = "halvcirkelknap4";
+            this.halvcirkelknap4.UseVisualStyleBackColor = true;
+            // 
+            // halvcirkelknap5
+            // 
+            this.halvcirkelknap5.Location = new System.Drawing.Point(855, 419);
+            this.halvcirkelknap5.Name = "halvcirkelknap5";
+            this.halvcirkelknap5.Size = new System.Drawing.Size(148, 119);
+            this.halvcirkelknap5.TabIndex = 7;
+            this.halvcirkelknap5.Text = "halvcirkelknap5";
+            this.halvcirkelknap5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 69);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Asiatisk Løve";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 616);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.halvcirkelknap5);
+            this.Controls.Add(this.halvcirkelknap4);
+            this.Controls.Add(this.halvcirkelknap3);
+            this.Controls.Add(this.halvcirkelknap2);
+            this.Controls.Add(this.halvcirkelknap1);
+            this.Controls.Add(this.nextKnap);
+            this.Controls.Add(this.prevKnap);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button prevKnap;
+        private System.Windows.Forms.Button nextKnap;
+        private Halvcirkelknap halvcirkelknap1;
+        private Halvcirkelknap halvcirkelknap2;
+        private Halvcirkelknap halvcirkelknap3;
+        private Halvcirkelknap halvcirkelknap4;
+        private Halvcirkelknap halvcirkelknap5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
